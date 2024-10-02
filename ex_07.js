10630f4c703b37aa9b3583a96b6496e08a1745d7
AlexCrt64 <alexandre.courtet@epitech.digital>
2024-10-02 19:22:58 +0200

    Exercice 7 Day 4

--- ex_07.js
+++ ex_07.js
@@ -1,55 +1,55 @@
 function createCity(country, cityName) {
-    let city = country.find(c => c[0] === cityName);
+    let city = country.find(c => c[0] === cityName)
     if (city) {
-        console.log(`La ville ${cityName} existe déjà.`);
-        return;
+        console.log(`La ville ${cityName} existe déjà.`)
+        return
     }
-    country.push([cityName, []]);
+    country.push([cityName, []])
 }
 function createHousing(country, cityName, housingName) {
-    let city = country.find(c => c[0] === cityName);
+    let city = country.find(c => c[0] === cityName)
     if (!city) {
-        console.log(`Échec de la création du logement : la ville ${cityName} n'existe pas.`);
-        return;
+        console.log(`Échec de la création du logement : la ville ${cityName} n'existe pas.`)
+        return
     }
-    let housing = city[1].find(h => h[0] === housingName);
+    let housing = city[1].find(h => h[0] === housingName)
     if (housing) {
-        console.log(`Le logement ${housingName} existe déjà dans ${cityName}.`);
-        return;
+        console.log(`Le logement ${housingName} existe déjà dans ${cityName}.`)
+        return
     }
-    city[1].push([housingName, []]);
+    city[1].push([housingName, []])
 }
 function addCharacteristics(country, cityName, housingName, ...characteristics) {
-    let city = country.find(c => c[0] === cityName);
+    let city = country.find(c => c[0] === cityName)
     if (!city) {
-        console.log(`Échec de l'ajout des caractéristiques : la ville ${cityName} n'existe pas.`);
-        return;
+        console.log(`Échec de l'ajout des caractéristiques : la ville ${cityName} n'existe pas.`)
+        return
     }
-    let housing = city[1].find(h => h[0] === housingName);
+    let housing = city[1].find(h => h[0] === housingName)
     if (!housing) {
-        console.log(`Échec de l'ajout des caractéristiques : le logement ${housingName} n'existe pas.`);
-        return;
+        console.log(`Échec de l'ajout des caractéristiques : le logement ${housingName} n'existe pas.`)
+        return
     }
-    housing[1].push(...characteristics);
+    housing[1].push(...characteristics)
 }
 function getCityOfCountry(country, cityName) {
-    let city = country.find(c => c[0] === cityName);
+    let city = country.find(c => c[0] === cityName)
     if (!city) {
-        console.log(`Échec de l'obtention de la ville : la ville ${cityName} n'existe pas.`);
-        return null;
+        console.log(`Échec de l'obtention de la ville : la ville ${cityName} n'existe pas.`)
+        return null
     }
-    return city;
+    return city
 }
 function getCharacteristicsOfHousing(country, cityName, housingName) {
-    let city = country.find(c => c[0] === cityName);
+    let city = country.find(c => c[0] === cityName)
     if (!city) {
-        console.log(`Échec de l'obtention des caractéristiques : la ville ${cityName} n'existe pas.`);
-        return null;
+        console.log(`Échec de l'obtention des caractéristiques : la ville ${cityName} n'existe pas.`)
+        return null
     }
-    let housing = city[1].find(h => h[0] === housingName);
+    let housing = city[1].find(h => h[0] === housingName)
     if (!housing) {
-        console.log(`Échec de l'obtention des caractéristiques : le logement ${housingName} n'existe pas.`);
-        return null;
+        console.log(`Échec de l'obtention des caractéristiques : le logement ${housingName} n'existe pas.`)
+        return null
     }
-    return housing[1];
+    return housing[1]
 }
