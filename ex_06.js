f1e6c449cbe7e4a8f2a95b37335e7e03de35aefb
AlexCrt64 <alexandre.courtet@epitech.digital>
2024-10-02 14:25:51 +0200

    Exercice 6 Day 4

---
+++ ex_06.js
@@ -0,0 +1,15 @@
+function findAddresses(toFind) {
+    const addresses = [
+        ["45 avenue des tulipes", "92130 Issy-les-Moulineaux"],
+        ["95 avenue parmentier", "75011 Paris"],
+        ["24 Rue Pasteur", "92400 Courbevoie"],
+        ["Champ de Mars, 5 Av. Anatole France", "75007 Paris"],
+        ["Pl. Louis-Armand", "75571 Paris"]
+    ];
+    addresses.forEach(address => {
+        let Address = address.join(', ')
+        if (Address.includes(toFind)) {
+            console.log(Address)
+        }
+    })
+}
