e65e5b78c7c246eab6fca49c6f3fec35dc4aded4
AlexCrt64 <alexandre.courtet@epitech.digital>
2024-10-02 11:32:17 +0200

    Exercice 2 Day 4

--- ex_02.js
+++ ex_02.js
@@ -1,11 +1,11 @@
 function findHousing(housingToFind,housingList){
-	let index=0
-	while (index<housingList.length){
-  	if (housingList[index].toLowerCase()===housingToFind.toLowerCase()){
-    	return [index,housingList[index]]
+  let index=0
+  while (index<housingList.length){
+    if (housingList[index].toLowerCase()===housingToFind.toLowerCase()){
+      return [index+1,housingList[index]]
     }
     else{
-    	index+=1
+      index+=1
     }
   }
   return null
