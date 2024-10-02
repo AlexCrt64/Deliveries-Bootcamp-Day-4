131772e4b054485ab9da9424e586d40cbac777b8
AlexCrt64 <alexandre.courtet@epitech.digital>
2024-10-02 13:09:45 +0200

    Exercice 5 Day 4

---
+++ ex_05.js
@@ -0,0 +1,8 @@
+function getHousesPrototype(){
+	let appartments = new Map();
+  appartments.set("Apartment 75008",90);
+  appartments.set("Apartment 94300",105);
+  appartments.set("Apartment 92300",140);
+  appartments.set("Apartment 75015",65);
+  return appartments
+}
