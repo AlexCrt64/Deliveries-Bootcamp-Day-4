c44d607bfe9641bdb54bc9f94d058e56bc6179f7
AlexCrt64 <alexandre.courtet@epitech.digital>
2024-10-02 19:16:24 +0200

    Exercice 7 Day 4

---
+++ ex_07.js
@@ -0,0 +1,55 @@
+function createCity(country, cityName) {
+    let city = country.find(c => c[0] === cityName);
+    if (city) {
+        console.log(`La ville ${cityName} existe déjà.`);
+        return;
+    }
+    country.push([cityName, []]);
+}
+function createHousing(country, cityName, housingName) {
+    let city = country.find(c => c[0] === cityName);
+    if (!city) {
+        console.log(`Échec de la création du logement : la ville ${cityName} n'existe pas.`);
+        return;
+    }
+    let housing = city[1].find(h => h[0] === housingName);
+    if (housing) {
+        console.log(`Le logement ${housingName} existe déjà dans ${cityName}.`);
+        return;
+    }
+    city[1].push([housingName, []]);
+}
+function addCharacteristics(country, cityName, housingName, ...characteristics) {
+    let city = country.find(c => c[0] === cityName);
+    if (!city) {
+        console.log(`Échec de l'ajout des caractéristiques : la ville ${cityName} n'existe pas.`);
+        return;
+    }
+    let housing = city[1].find(h => h[0] === housingName);
+    if (!housing) {
+        console.log(`Échec de l'ajout des caractéristiques : le logement ${housingName} n'existe pas.`);
+        return;
+    }
+    housing[1].push(...characteristics);
+}
+function getCityOfCountry(country, cityName) {
+    let city = country.find(c => c[0] === cityName);
+    if (!city) {
+        console.log(`Échec de l'obtention de la ville : la ville ${cityName} n'existe pas.`);
+        return null;
+    }
+    return city;
+}
+function getCharacteristicsOfHousing(country, cityName, housingName) {
+    let city = country.find(c => c[0] === cityName);
+    if (!city) {
+        console.log(`Échec de l'obtention des caractéristiques : la ville ${cityName} n'existe pas.`);
+        return null;
+    }
+    let housing = city[1].find(h => h[0] === housingName);
+    if (!housing) {
+        console.log(`Échec de l'obtention des caractéristiques : le logement ${housingName} n'existe pas.`);
+        return null;
+    }
+    return housing[1];
+}
